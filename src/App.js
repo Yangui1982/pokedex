543027eaf734a0f63dd0a2bd5ba5c2f0efb90ee7
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2023-08-07 00:23:23 +0200

    style de la pagination

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ import './App.css';
 
 // export default App;
 
+
 const PokemonCard = ({ name, url }) => {
   const [pokemonData, setPokemonData] = useState(null);
 
@@ -95,7 +96,7 @@ const PokemonCard = ({ name, url }) => {
     fetchData();
   }, [url]);
 
-  return (
+    return (
     <div className="card">
       {pokemonData ? (
         <>
@@ -123,12 +124,12 @@ const PokemonCard = ({ name, url }) => {
 const App = () => {
   const [pokemonData, setPokemonData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const cardsPerPage = 6;
+  const cardsPerPage = 8;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1281');
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1273');
         const data = await response.json();
         setPokemonData(data.results);
       } catch (error) {
@@ -139,12 +140,43 @@ const App = () => {
     fetchData();
   }, []);
 
-  const indexOfLastCard = currentPage * cardsPerPage;
-  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-  const currentPokemonData = pokemonData.slice(indexOfFirstCard, indexOfLastCard);
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+    // Calculer le nombre total de pages
+    const totalPages = Math.ceil(pokemonData.length / cardsPerPage);
+
+    // Fonction pour changer de page
+    const handlePageChange = (pageNumber) => {
+      setCurrentPage(pageNumber);
+    };
+
+    // Générer les numéros de page pour la pagination
+    const renderPageNumbers = () => {
+      const pageNumbers = [];
+      for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+      }
+
+      // Pour limiter le nombre de numéros de page affichés, vous pouvez adapter cette partie selon vos préférences.
+      const visiblePageNumbers = pageNumbers.filter(
+        (pageNumber) =>
+          pageNumber === currentPage ||
+          (pageNumber >= currentPage - 1 && pageNumber <= currentPage + 1)
+      );
+
+      return visiblePageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => handlePageChange(pageNumber)}
+          className={currentPage === pageNumber ? 'active' : ''}
+        >
+          {pageNumber}
+        </button>
+      ));
+    };
+
+    // Calculer l'index de début et de fin des cartes à afficher
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentPokemonData = pokemonData.slice(indexOfFirstCard, indexOfLastCard);
   return (
     <div>
       <div className="cards">
@@ -152,12 +184,19 @@ const App = () => {
           <PokemonCard key={index} name={pokemon.name} url={pokemon.url} />
         ))}
       </div>
-      <div>
-        {currentPage > 1 && (
-          <button onClick={() => handlePageChange(currentPage - 1)}>Page précédente</button>
+      <div className="pages">
+      {currentPage >= 1 && (
+          <>
+            <button onClick={() => handlePageChange(1)}>&laquo;</button>
+            <button onClick={() => handlePageChange(currentPage - 1)}>&lt;</button>
+          </>
         )}
-        {currentPage < Math.ceil(pokemonData.length / cardsPerPage) && (
-          <button onClick={() => handlePageChange(currentPage + 1)}>Page suivante</button>
+        {renderPageNumbers()}
+        {currentPage <= totalPages && (
+          <>
+            <button onClick={() => handlePageChange(currentPage + 1)}>&gt;</button>
+            <button onClick={() => handlePageChange(totalPages)}>&raquo;</button>
+          </>
         )}
       </div>
     </div>
