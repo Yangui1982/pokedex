acfb4a42baf1b363b8bae976e24fa4203024544a
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2023-08-20 22:45:59 +0200

    components creation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import "./component";
 import './App.css';
 // import axios from "axios";
 
@@ -79,47 +80,47 @@ import './App.css';
 // export default App;
 
 
-const PokemonCard = ({ name, url }) => {
-  const [pokemonData, setPokemonData] = useState(null);
+// const PokemonCard = ({ name, url }) => {
+//   const [pokemonData, setPokemonData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setPokemonData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         setPokemonData(data);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//       }
+//     };
 
-    fetchData();
-  }, [url]);
+//     fetchData();
+//   }, [url]);
 
-    return (
-    <div className="card">
-      {pokemonData ? (
-        <>
-          <div className="title">
-            <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
-          </div>
-          <div className="data">
-            <div className="profil">
-              <img src={pokemonData.sprites.front_default} alt={name} />
-            </div>
-            <div className="infos">
-              <p>Types: {pokemonData.types.map((type) => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)).join(', ')}</p>
-              <p>Height: {pokemonData.height}</p>
-              <p>Weight: {pokemonData.weight}</p>
-            </div>
-          </div>
-        </>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
-};
+//     return (
+//     <div className="card">
+//       {pokemonData ? (
+//         <>
+//           <div className="title">
+//             <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+//           </div>
+//           <div className="data">
+//             <div className="profil">
+//               <img src={pokemonData.sprites.front_default} alt={name} />
+//             </div>
+//             <div className="infos">
+//               <p>Types: {pokemonData.types.map((type) => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)).join(', ')}</p>
+//               <p>Height: {pokemonData.height}</p>
+//               <p>Weight: {pokemonData.weight}</p>
+//             </div>
+//           </div>
+//         </>
+//       ) : (
+//         <p>Loading...</p>
+//       )}
+//     </div>
+//   );
+// };
 
 const App = () => {
   const [pokemonData, setPokemonData] = useState([]);
